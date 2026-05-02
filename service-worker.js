0c6e91fb679410934a4e5c0f4bf30000b511f55d
qbone967 <burrow_blocker.9o@icloud.com>
2026-05-02 14:05:55 +0200

    Add files via upload
    Bigger fonts

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // Auto Hájek · Garance PWA — Service Worker
 // Cachuje všechny potřebné soubory pro offline režim
 
-const CACHE_NAME = 'garance-v9';
+const CACHE_NAME = 'garance-v10';
 const FILES_TO_CACHE = [
   './',
   './index.html',
